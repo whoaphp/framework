388c429a1eaa7895a64ff105480e92adbb9d2db2
dreamsbond <info@dreamsbond.com>
2021-05-05 11:27:21 +0800

    Add component JsonWebToken.

---
+++ components/JsonWebToken/src/Checkers/TenantChecker.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Copyright 2021 info@whoaphp.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace Limoncello\JsonWebToken\Checkers;
+
+use Jose\Component\Checker\ClaimChecker;
+use Jose\Component\Checker\InvalidClaimException;
+
+/**
+ * @package Limoncello\JsonWebToken
+ */
+class TenantChecker implements ClaimChecker
+{
+    /** @var string */
+    private const CLAIM_NAME = 'tid';
+
+    /**
+     * @var string
+     */
+    private string $tenant;
+
+    /**
+     * @var bool
+     */
+    private bool $protectedHeader;
+
+    /**
+     * @param string $tenant
+     * @param bool   $protectedHeader
+     */
+    public function __construct(string $tenant, bool $protectedHeader = false)
+    {
+        $this->tenant          = $tenant;
+        $this->protectedHeader = $protectedHeader;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function checkClaim($value): void
+    {
+        $this->checkValue($value, InvalidClaimException::class);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supportedClaim(): string
+    {
+        return self::CLAIM_NAME;
+    }
+
+    /**
+     * @param        $value
+     * @param string $class
+     */
+    private function checkValue($value, string $class): void
+    {
+        if (is_string($value) === true && $value !== $this->tenant) {
+            throw new $class('Bad tenant.', self::CLAIM_NAME, $value);
+        }
+        if (is_array($value) === true && in_array($this->tenant, $value, true) === false) {
+            throw new $class('Bad tenant.', self::CLAIM_NAME, $value);
+        }
+        if (is_array($value) === false && is_string($value) === false) {
+            throw new $class('Bad tenant.', self::CLAIM_NAME, $value);
+        }
+    }
+}
---
+++ components/JsonWebToken/tests/TenantCheckerTest.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Copyright 2021 info@whoaphp.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace Limoncello\Tests\JsonWebToken;
+
+use Jose\Component\Checker\InvalidClaimException;
+use Limoncello\JsonWebSignature\Checkers\TenantChecker;
+
+/**
+ * @package Limoncello\Tests\JsonWebToken
+ */
+class TenantCheckerTest extends TestCase
+{
+    /**
+     * Test tenant claim 1
+     */
+    public function testTenantClaim1(): void
+    {
+        $this->expectException(InvalidClaimException::class);
+        $this->expectExceptionMessage('Bad tenant.');
+
+        $checker = new TenantChecker('foo');
+        $checker->checkClaim(1);
+    }
+
+    /**
+     * Test tenant claim 2
+     */
+    public function testTenantClaim2(): void
+    {
+        $this->expectException(InvalidClaimException::class);
+        $this->expectExceptionMessage('Bad tenant.');
+
+        $checker = new TenantChecker('foo');
+        $checker->checkClaim('bar');
+    }
+
+    /**
+     * Test tenant claim 3
+     */
+    public function testTenantClaim3(): void
+    {
+        $this->expectException(InvalidClaimException::class);
+        $this->expectExceptionMessage('Bad tenant.');
+
+        $checker = new TenantChecker('foo');
+        $checker->checkClaim(['bar']);
+    }
+
+    /**
+     * Test tenant claim 4
+     */
+    public function testTenantClaim4(): void
+    {
+        $checker = new TenantChecker('foo');
+        $checker->checkClaim('foo');
+        $checker->checkClaim(['foo']);
+        static::assertEquals('tid', $checker->supportedClaim());
+    }
+}
